eeeef82e56332acb819312a9216dfe658fe582c3
ErlBot <104343622+ErlBot@users.noreply.github.com>
2022-04-25 13:34:43 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,55 @@
+let fs = require('fs')
+let chalk = require('chalk')
+
+global.owner = ['6281232373549' , '628885813800'] // Put your number here
+global.mods = ['6289612068701'] // Want some help?
+global.prems = [''] // Premium user has unlimited limit
+global.APIs = { // API Prefix
+  // name: 'https://website'
+  nrtm: 'https://nurutomo.herokuapp.com',
+  xteam: 'https://api.xteam.xyz',
+  zahir: 'https://zahirr-web.herokuapp.com',
+  zeks: 'https://api.zeks.xyz',
+  pencarikode: 'https://pencarikode.xyz',
+  LeysCoder: 'https://leyscoders-api.herokuapp.com'
+}
+global.APIKeys = { // APIKey Here
+  // 'https://website': 'apikey'
+  'https://api.xteam.xyz': 'your_apikey',
+  'https://zahirr-web.herokuapp.com': 'zahirgans',
+  'https://api.zeks.xyz': 'apivinz',
+  'https://pencarikode.xyz': 'pais',
+  'https://leyscoders-api.herokuapp.com': 'MIMINGANZ'
+}
+// Sticker WM
+
+const spack = fs.readFileSync("lib/exif.json")
+const stickerpack = JSON.parse(spack)
+if (stickerpack.spackname == '') {
+  var sticker_name = 'ErlBot'
+  var sticker_author = 'by YahahaErlangga\nwa.me/6281232373549'
+} else {
+  var sticker_name = stickerpack.spackname
+  var sticker_author = stickerpack.sauthor
+}
+
+const file_exif = "lib/exif.json"
+fs.watchFile(file_exif, () => {
+  fs.unwatchFile(file_exif)
+  console.log(chalk.redBright("Update 'exif.json'"))
+  delete require.cache[file_exif]
+  require('./lib/exif.json')
+})
+
+global.packname = sticker_name
+global.author = sticker_author
+
+global.multiplier = 29 // The higher, The harder levelup
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+  fs.unwatchFile(file)
+  console.log(chalk.redBright("Update 'config.js'"))
+  delete require.cache[file]
+  require(file)
+})
